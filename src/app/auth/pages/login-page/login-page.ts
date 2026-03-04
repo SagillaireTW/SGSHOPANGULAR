7122f921a919d97ce12c78f72697494b947ed5c5
SagillaireTW <juansantis@twentic.com>
2026-03-04 12:24:59 +0100

    271. Formulario reactivo de autenticación

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.html',
 })
-export class LoginPage { }
+export class LoginPage {
+  fb = inject(FormBuilder);
+  hasError = signal<boolean>(false);
+  isPosting = signal<boolean>(false);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+    console.log({ email, password });
+
+  }
+}
