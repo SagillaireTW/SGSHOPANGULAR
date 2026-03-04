4ed9d7515cef7e11ecc1d8f6023519a80338696d
SagillaireTW <juansantis@twentic.com>
2026-03-04 10:39:41 +0100

    261. Paginación con RxResource

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { ProductCard } from "../../../products/components/product-card/product-card";
 import { ProductService } from '../../../products/services/products.service';
-import { Product } from '../../../products/interfaces/product.interface';
 import { Pagination } from "../../../shared/components/pagination/pagination";
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -12,9 +13,21 @@ import { Pagination } from "../../../shared/components/pagination/pagination";
 })
 export class HomePage {
   private productService = inject(ProductService);
+  activatedRoute = inject(ActivatedRoute);
+
+  currentPage = toSignal(this.activatedRoute.queryParamMap.pipe(
+    map((params) => params.get('page') ? +params.get('page')! : 1),
+    map((page) => isNaN(page) ? 1 : page)
+  ),
+    {
+      initialValue: 1
+    }
+  );
 
   productResource = rxResource({
-    params: () => ({}),
-    stream: ({ params }) => this.productService.getProducts({})
+    params: () => ({ page: this.currentPage() - 1 }),
+    stream: ({ params }) => this.productService.getProducts({
+      offset: params.page * 9,
+    })
   })
 }
