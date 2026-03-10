0d1d7a79d0b38b382ecaa89db1be8d53cdb7b93d
Juan Santis <juansantis@twentic.com>
2026-03-10 10:50:21 +0100

    293. Formulario de producto

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,14 +1,48 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { Product } from '../../../../products/interfaces/product.interface';
 import { ProductSwiper } from "../../../../products/components/product-swiper/product-swiper";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductSwiper],
+  imports: [ProductSwiper, ReactiveFormsModule],
   templateUrl: './product-details.html',
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit {
   product = input.required<Product>();
 
-  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+  fb = inject(FormBuilder)
+
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['', [
+      Validators.required,
+      Validators.pattern(FormUtils.slugPattern),
+
+    ]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kids|unisex/)]]
+  });
+
+  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit() {
+    this.setFormValue(this.product())
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    this.productForm.reset(this.product() as any)
+    // this.productForm.patchValue(formLike as any)
+    this.productForm.patchValue({ tags: formLike.tags?.join(', ') })
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
 }
